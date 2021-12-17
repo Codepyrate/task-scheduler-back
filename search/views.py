4c561d251fc2ad137ef06e4267f34494f0a5e553
Suzan-Hiary <suze-hiary@hotmail.com>
2021-12-18 00:15:30 +0200

    change the api

--- search/views.py
+++ search/views.py
@@ -13,7 +13,7 @@ def get_query(req):
         q = req.query_params.get('q')
         print(q)
 
-        url = f"https://serpapi.com/search.json?q={q}&location=Austin%2C+Texas%2C+United+States&hl=en&gl=us&google_domain=google.com&api_key=48cb4bc06fef6a9897d8bec1d2a4724e475c9bbe1b4cbf19e2c7555f3ca4ea25"
+        url = f"https://serpapi.com/search.json?q={q}&location=Austin%2C+Texas%2C+United+States&hl=en&gl=us&google_domain=google.com&api_key=1bd1bc394ab69a581ecbd7ba87230e8c1473514be84f0012ebc949df8d827b2b"
 
         response = requests.request("GET", url, headers=headers, data=payload)
     return Response(response.json())
